8a3897d80e5ee94c35dd7d32a877b3533100497b
Yashika Tomar <yashikatomar@Yashikas-MacBook-Air.local>
2026-04-08 11:00:58 +0530

    fix app.py for render deployment

--- app.py
+++ app.py
@@ -22,3 +22,9 @@ def predict(data: InputData):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
